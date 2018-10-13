7faef97a96d18b3f1c61df8846f5cedb2a1f7301
Sergii Kolisnyk <sergii_kolisnyk@epam.com>
2018-10-13 19:39:20 +0000

    [Client] Add API service

---
+++ client/src/api/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { BASE_URL } from '../config';
+
+const instance = axios.create({
+  baseURL: BASE_URL
+});
+
+const getCampaigns = () => instance.get('/campaigns');
+
+export default {
+  getCampaigns
+}
