ca5e013adb855e8de2017fc38b0101e4ed07a821
Sergii Kolisnyk <sergii_kolisnyk@epam.com>
2018-10-24 16:13:14 +0000

    [Client] Remove redundant <div/>

--- client/src/components/campaigns/list/item/item.js
+++ client/src/components/campaigns/list/item/item.js
@@ -10,38 +10,36 @@ import { renderStatusIcon, renderPlatformIcon } from './../../../helpers';
 
 import styles from './styles';
 
-const CampaignListItem = ({ campaign, classes }) => (
-  <div>
-    <Link className={ classes.link } to={ `/campaigns/${campaign.id}` }>
-      <Paper className={ classes.paper }>
-        <Grid item container justify="space-between"> 
-          <Grid item>
-            { 
-              renderStatusIcon(campaign.status, classes) 
-            }
-          </Grid>
-          <Grid item>         
-            <Typography variant="h6" noWrap>{ campaign.name }</Typography>
-            <Typography variant="subtitle1" color="textSecondary" noWrap>{ campaign.goal }</Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="subtitle2" align="right" className={ classes.price }>€{ campaign.total_budget }</Typography>
-            <ul className={ classes.platformIconsList }>
-              { 
-                Object.keys(campaign.platforms).map(platform => (
-                  <li key={ platform } >
-                    <img className={ classes.platformIcon } 
-                      alt={ platform }
-                      src={ renderPlatformIcon(platform) }/>
-                  </li>
-                ))
-              }
-            </ul>
-          </Grid>
+const CampaignListItem = ({ campaign, classes }) => (  
+  <Link className={ classes.link } to={ `/campaigns/${campaign.id}` }>
+    <Paper className={ classes.paper }>
+      <Grid item container justify="space-between"> 
+        <Grid item>
+          { 
+            renderStatusIcon(campaign.status, classes) 
+          }
         </Grid>
-      </Paper>
-    </Link>   
-  </div>
+        <Grid item>         
+          <Typography variant="h6" noWrap>{ campaign.name }</Typography>
+          <Typography variant="subtitle1" color="textSecondary" noWrap>{ campaign.goal }</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="subtitle2" align="right" className={ classes.price }>€{ campaign.total_budget }</Typography>
+          <ul className={ classes.platformIconsList }>
+            { 
+              Object.keys(campaign.platforms).map(platform => (
+                <li key={ platform } >
+                  <img className={ classes.platformIcon } 
+                    alt={ platform }
+                    src={ renderPlatformIcon(platform) }/>
+                </li>
+              ))
+            }
+          </ul>
+        </Grid>
+      </Grid>
+    </Paper>
+  </Link>   
 );
 
 CampaignListItem.propTypes = {
